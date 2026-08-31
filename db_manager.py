37c50ea74a01c5461dbfc617c6ed7c6867551906
abhaymakes <fallencat.user@gmail.com>
2026-08-31 17:09:16 +0530

    db_manager to perform database operations

---
+++ db_manager.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+from uuid import uuid4
+
+from sqlalchemy import create_engine, String, DateTime, Boolean
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session
+
+engine = create_engine("sqlite:///c2.db")
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Beacon(Base):
+    __tablename__ = "beacons"
+
+    id: Mapped[str] = mapped_column(String, primary_key=True)
+    public_key: Mapped[str] = mapped_column(String)
+    system_info: Mapped[str] = mapped_column(String)
+    registered_at: Mapped[datetime] = mapped_column(DateTime)
+    last_active: Mapped[datetime] = mapped_column(DateTime)
+    status: Mapped[str] = mapped_column(String, default="offline")
+
+class Challenge(Base):
+    __tablename__ = "challenges"
+
+    challenge_id: Mapped[str] = mapped_column(String, primary_key=True)
+    challenge: Mapped[str] = mapped_column(String)
+    used: Mapped[bool] = mapped_column(Boolean, default=False)
+    
+
+
+Base.metadata.create_all(engine)
+
+
+def get_session():
+    return Session(engine)
